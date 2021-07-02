5f843c59893c9e73ef7c4e57f72a23d88c88c2fe
rama41222 <dinushankanrg@gmail.com>
2021-07-03 05:24:05 +0530

    feat: fetching weather on route

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv'
+import { fetchWeather } from './services'
 
+dotenv.config();
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Well done!!');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', async(req, res) => {
+    const data = await fetchWeather(process.env.WEATHER_API as string);
+    res.status(200).json(data);
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log('The application is listening on port 3000!');
 });
