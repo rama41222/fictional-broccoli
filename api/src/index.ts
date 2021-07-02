7db5fcc5995372494de6d4008261970579f2f91e
rama41222 <dinushankanrg@gmail.com>
2021-07-03 03:31:56 +0530

    feat: basic server

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello world!');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Well done!!');
+});
+
+app.listen(3000, () => {
+    console.log('The application is listening on port 3000!');
+});
