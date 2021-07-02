42d78b9911ff2d3cc70792379e243d8000be93d7
rama41222 <dinushankanrg@gmail.com>
2021-07-03 04:47:01 +0530

    refactor: removed unnecessary code

---
+++ dashboard/src/dashboard/dashboard.tsx
@@ -0,0 +1,14 @@
+import './dashboard.css';
+
+function Dashboard() {
+
+  return (
+    <div className="dashboard">
+        <h1>
+          Hello React
+        </h1>
+    </div>
+  );
+}
+
+export default Dashboard;
