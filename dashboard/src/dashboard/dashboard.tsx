519eb479136b9412391e56127a1ccd538565925d
rama41222 <dinushankanrg@gmail.com>
2021-07-05 22:25:45 +0530

    feat: dashboard form

--- dashboard/src/dashboard/dashboard.tsx
+++ dashboard/src/dashboard/dashboard.tsx
@@ -1,13 +1,45 @@
-import './dashboard.css';
+import "./dashboard.css";
+import { useEffect } from 'react'
+
 
 function Dashboard() {
 
+  useEffect(()=> {
+    console.log('initial api call')
+  },[])
+
   return (
-    <div className="dashboard">
-        <h1>
-          Hello React
-        </h1>
-    </div>
+    <>
+      <div className="dashboard">
+        <h1>Dashboard</h1>
+      </div>
+      <div>
+        <form>
+          <div>
+            <input type="text" placeholder="From" />
+          </div>
+          <div>
+            <input type="text" placeholder="to" />
+          </div>
+          <div>
+            <input type="text" placeholder="at" />
+          </div>
+          <div>
+            <label>
+              Select the frequency
+              <select>
+                <option value="hourly">Hourly</option>
+                <option value="daily">Daily</option>
+              </select>
+            </label>
+          </div>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+      <div>
+        Your Data here
+      </div>
+    </>
   );
 }
 
